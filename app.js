a3d94d1bcc7c45ca93d2b6adb84c77c71e8fab99
dyslab <45236395+dyslab@users.noreply.github.com>
2019-06-20 04:00:48 +0800

    Base node.js website framework.  📌

---
+++ app.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+const path = require('path');
+const createError = require('http-errors');
+const express = require('express');
+const app = express();
+
+const APP_FOLDER = 'dist/';
+const APP_PORT = 8060;
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'server', 'views'));
+app.use('/dist', express.static(APP_FOLDER));
+
+app.get('/', (req, res) => {
+  res.render('index', { title:'INDEX' });
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', { title: 'ERROR' });
+})
+
+console.log('Web Server Started.');
+console.log(`Link: [http://localhost:${APP_PORT}]`);
+console.log(`Ctrl+C to Exit...`);
+
+app.listen(APP_PORT);
