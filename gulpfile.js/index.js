862d96f0bbe0ae34cb649d87fd0db074a99a987a
dyslab <45236395+dyslab@users.noreply.github.com>
2019-06-24 06:47:02 +0800

    Daily Learning Update #0624 🕰

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -13,34 +13,44 @@ function babel2js() {
     .pipe(dest('dist/assets/js/'));
 }
   
+// =============================================================================
+//
+// Pug数据初始化部分，开始
+//
+// news数据处理：从json文件读取news列表
+const news_list = JSON.parse(fs.readFileSync('./src/news/news_list.json'));
+let news_jsons = []
+for(id in news_list) {
+    let newsitem = JSON.parse(fs.readFileSync('./src/news/' + news_list[id] + '.json'))
+    newsitem.id = news_list[id]
+    news_jsons.push(newsitem);
+}
+//
+// 数据初始化部分，结束
+//
+// =============================================================================
+
 // 处理PUG模板文件（index.pug）
 function index2html() {
+    // console.log(news_list);
     return src('src/*.pug')
-    .pipe(pug())
+    .pipe(pug({
+        locals : { data: { newsjsons: news_jsons.slice(0, 6) } }
+    }))
     .pipe(dest('dist/'));
 }
-
-// ----------------------------------------------------------
-//
-// news部分开始
-//
-const news_list = JSON.parse(fs.readFileSync('./src/news/news_list.json'));
 
 // 处理PUG模板文件（news）
 function news2html(cb) {
     for(id in news_list) {
         src('src/news/news_doc.pug')
         .pipe(pug({
-            data : { data: JSON.parse(fs.readFileSync('./src/news/' + news_list[id] + '.json')) }
+            locals : { data: news_jsons[id] }
         }))
         .pipe(rename({ basename: news_list[id] }))
         .pipe(dest('dist/'));
         cb();
     }
 }
-//
-// news部分结束
-//
-// ----------------------------------------------------------
 
 exports.default = parallel(babel2js, index2html, news2html);
